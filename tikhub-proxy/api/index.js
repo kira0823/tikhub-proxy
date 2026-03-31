fc2394e334b79cf6b9ba283e73d88301024f1df3
kira0823 <1335112186@qq.com>
2026-03-31 18:17:59 +0800

    Update index.js

--- tikhub-proxy/api/index.js
+++ tikhub-proxy/api/index.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   const url = req.url || '/'
   const target = 'https://api.tikhub.io' + url.replace(/^\/api\/index/, '').replace(/^\/api/, '')
 
@@ -12,13 +12,15 @@ export default function handler(req, res) {
     headers['Authorization'] = auth
   }
 
-  return fetch(target, { method: 'GET', headers })
-    .then(resp => resp.text().then(data => {
-      res.setHeader('Access-Control-Allow-Origin', '*')
-      res.setHeader('Content-Type', 'application/json')
-      res.status(resp.status).send(data)
-    }))
-    .catch(e => {
+  fetch(target, { method: 'GET', headers: headers })
+    .then(function(resp) {
+      return resp.text().then(function(data) {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Content-Type', 'application/json')
+        res.status(resp.status).send(data)
+      })
+    })
+    .catch(function(e) {
       res.status(500).json({ error: e.message })
     })
 }
